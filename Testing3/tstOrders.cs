7b0f7a0c50d6f14566d0f190dcdb9f075602af2f
P2681686 <P2681686@GWHXX682WIN016W>
2023-02-06 15:03:13 +0000

    Added testing for order class

---
+++ Testing3/tstOrders.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrder anOrder = new clsOrder();
+            Assert.IsNotNull(anOrder);
+        }
+    }
+}
