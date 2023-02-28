61c4c8dd8c0b3bafa989a0f7ea6aec05a13d55b7
P2687532 <P2687532@my365.dmu.ac.uk>
2023-02-28 01:33:16 +0000

    Added testing for Stock class

---
+++ Testing1/tstStock.cs
@@ -0,0 +1,73 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock aStock = new clsStock();
+            Assert.IsNotNull(aStock);
+        }
+
+        [TestMethod]
+        public void StockNoPropertyOk()
+        {
+            clsStock aStock = new clsStock();
+            Int32 TestData = 1;
+            aStock.StockNo = TestData;
+            Assert.AreEqual(aStock.StockNo, TestData);
+        }
+
+        [TestMethod]
+        public void StockNamePropertyOk()
+        {
+            clsStock aStock = new clsStock();
+            string TestData = "Some Name";
+            aStock.StockName = TestData;
+            Assert.AreEqual(aStock.StockName, TestData);
+        }
+
+        [TestMethod]
+        public void DescriptionPropertyOk()
+        {
+            clsStock aStock = new clsStock();
+            string TestData = "Description";
+            aStock.Description = TestData;
+            Assert.AreEqual(aStock.Description, TestData);
+        }
+
+        [TestMethod]
+        public void StockQuantityPropertyOk()
+        {
+            clsStock aStock = new clsStock();
+            Int32 TestData = 1;
+            aStock.StockQuantity = TestData;
+            Assert.AreEqual(aStock.StockQuantity, TestData);
+        }
+
+        [TestMethod]
+        public void ProcessedDatePropertyOk()
+        {
+            clsStock aStock = new clsStock();
+            DateTime TestData = DateTime.Now.Date;
+            aStock.ProcessedDate = TestData;
+            Assert.AreEqual(aStock.ProcessedDate, TestData);
+        }
+
+        [TestMethod]
+        public void AvailablePropertyOk()
+        {
+            clsStock aStock = new clsStock();
+            Boolean TestData = true;
+            aStock.Available = TestData;
+            Assert.AreEqual(aStock.Available, TestData);
+        }
+
+    }
+}
+
