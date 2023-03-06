c170ee6cb135808fa2b54ffc96a92784b089f6d2
Sumedh Singare <P2687208@my365.dmu.ac.uk>
2023-03-06 13:06:37 +0000

    Done some coding.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,23 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public DateTime DateAdded { get; set; }
+        public bool Active { get; set; }
+        public string StaffFirstName { get; set; }
+
+        public clsStaff
+            {
+            }
+    public int staffId { get; set;}
+    public string staffFirstName { get; set; }
+    public string staffLastName { get; set; }
+    public string staffRole { get; set; }
+    public date staffDateOfBirth { get; set; }
+    public text staffDepartment { get; set; }
+    public bool staffAdmin { get; set; }
+
     }
 }
