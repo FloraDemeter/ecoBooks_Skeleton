4ab6caec41d46b3b777c1fe564af27aea5ae478a
Diviyesh Venkatachalam <P2693734@my365.dmu.ac.uk>
2023-03-07 20:31:41 +0000

    added testing for supplier class

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        public object SupplierNo { get; set; }
+        public string SupplierName { get; set; }
+        public DateTime SupplyDate { get; set; }
+        public bool UK { get; set; }
+        public string SupplierAddress { get; set; }
+        public int SupplierContactNO { get; set; }
     }
 }
