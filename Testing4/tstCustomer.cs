3f39c4c83820f4370c4472440f9693b78e64e6c8
Mohammed Kareem <P2625212@my365.dmu.ac.uk>
2023-03-16 14:48:27 +0000

    16th

---
+++ Testing4/tstCustomer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
