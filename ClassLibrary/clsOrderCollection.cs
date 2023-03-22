53270b68cec58ef6b19da127a09d67c23c4424bc
P2681686 <P2681686@GWHXX682WIN016W>
2023-03-22 10:45:38 +0000

    Working OrderCollection

---
+++ ClassLibrary/clsOrderCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsOrderCollection
+    {
+       
+        List<clsOrders> mOrderList = new List<clsOrders>();
+        public clsOrderCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblOrder_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsOrders AnOrder = new clsOrders();
+                AnOrder.OrderID = Convert.ToInt32(DB.DataTable.Rows[0]["OrderID"]);
+                AnOrder.OrderDate = Convert.ToDateTime(DB.DataTable.Rows[0]["OrderDate"]);
+                AnOrder.CustomerNo = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerNo"]);
+                AnOrder.CustomerName = Convert.ToString(DB.DataTable.Rows[0]["CustomerName"]);
+                AnOrder.StockNo = Convert.ToInt32(DB.DataTable.Rows[0]["StockNo"]);
+                AnOrder.StockPrice = Convert.ToDouble(DB.DataTable.Rows[0]["StockPrice"]);
+                AnOrder.OrderConfirmed = Convert.ToBoolean(DB.DataTable.Rows[0]["OrderConfirmed"]);
+                mOrderList.Add(AnOrder);
+                Index++;
+            }
+        }
+        public List<clsOrders> OrderList 
+        { 
+            get
+            {
+                return mOrderList;
+            }
+            set
+            {
+                mOrderList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mOrderList.Count;
+            }
+            set
+            {
+
+            } 
+        }
+        public clsOrders ThisOrder { get; set; }
+    }
+}
