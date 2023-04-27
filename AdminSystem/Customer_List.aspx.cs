8e05b883412894be32c0ddbc788c53a617a86c13
Mohammed Kareem <P2625212@my365.dmu.ac.uk>
2023-04-27 20:26:58 +0100

    ADDED PRPERTIES COLLECTION DONE

--- AdminSystem/Customer_List.aspx.cs
+++ AdminSystem/Customer_List.aspx.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            DisplayCustomer();
+        }
 
+    }
+
+     void DisplayCustomer()
+    {
+        clsCustomerCollection Customer = new clsCustomerCollection();
+        lstCustomerlist.DataSource = Customer.customerlist;
+        lstCustomerlist.DataValueField = "CustomerID";
+        lstCustomerlist.DataTextField = "CustomerFullName";
+        lstCustomerlist.DataBind();
     }
 }
---
+++ ClassLibrary/clsCustomerCollections.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        public clsCustomerCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            //get the count of records
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sprox_tblCustomer_SelectALL");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                //creating a blank address
+                clsCustomer customer = new clsCustomer();
+                //read the fields from the current record
+                customer.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                customer.CustomerFullName = Convert.ToString(DB.DataTable.Rows[Index]["CustomerFullName"]);
+                customer.CustomerAddress = Convert.ToString(DB.DataTable.Rows[Index]["CustomerAddress"]);
+                customer.CustomerEmailAddress = Convert.ToString(DB.DataTable.Rows[Index]["CustomerEmailAddress"]);
+                customer.CustomerDateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[Index]["CustomerDateOfBirth"]);
+                customer.CustomerLoyalty = Convert.ToBoolean(DB.DataTable.Rows[Index]["CustomerLoyalty"]);
+                //add the record to the private data member
+                mCustomerList.Add(customer);
+                //point of the next record
+                Index++;
+            }
+
+        }
+
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        public List<clsCustomer> customerlist
+        {
+            get
+            {
+                //return the private data
+                return mCustomerList;
+            }
+            set
+            {
+                //set the private data 
+                mCustomerList = value;
+            }
+        }
+        public clsCustomer thiscustomer { get; set; }
+        public int Count
+        {
+            get { return mCustomerList.Count; }
+            set { } //set up later on
+        }
+   }
+}
+
+ 
